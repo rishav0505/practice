1b6852fb206613085e272a1d3661c2ea177d38ad
Rishav Saha <98244985+rishav0505@users.noreply.github.com>
2022-10-09 19:14:40 +0530

    adding search() and deletion operations with time complexity

--- dsa/linked list/circular linked list/c-singly-basics.c
+++ dsa/linked list/circular linked list/c-singly-basics.c
@@ -8,10 +8,44 @@ struct node
     struct node *next;
 };
 
+// ---count no. of nodes---
+void countNodes(struct node *tail)
+{
+    int i = 0;
+    struct node *temp = tail->next;
+    do
+    {
+        i++;
+        temp = temp->next;
+    } while (temp != tail->next);
+    printf("\nList Length = %d", i);
+}
+
+// ---search an element in the list---
+void search(struct node *tail, int data)
+{
+    if (tail == NULL)
+        printf("\nLinked list is empty!");
+    int i = 0;
+    struct node *temp = tail->next;
+    do
+    {
+        i++;
+        if (temp->data == data)
+        {
+            printf("\n%d found in index %d!", data, i);
+            return;
+        }
+        temp = temp->next;
+    } while (temp != tail->next);
+    printf("\n%d didn't found in any of the node.", data);
+}
+
 // ---prints the linked list--- Time Complexity = O(n)
 void print(struct node *tail)
 {
     struct node *ptr = tail->next;
+    printf("\nData = ");
     do
     {
         printf("%d ", ptr->data);
@@ -94,18 +128,95 @@ struct node *addBeforePos(struct node *tail, int data, int pos)
     return tail;
 }
 
+// ---delete first node of the list---
+struct node *delFirstNode(struct node *tail)
+{
+    if (tail == NULL || tail->next == tail)
+    {
+        free(tail);
+        tail = NULL;
+        return NULL;
+    }
+
+    struct node *temp;
+    temp = tail->next;
+    tail->next = temp->next;
+    free(temp);
+    temp = NULL;
+
+    return tail;
+}
+
+// ---delete last node of the list---
+struct node *delLastNode(struct node *tail)
+{
+    struct node *temp1;
+    temp1 = tail->next;
+    while (temp1->next != tail)
+        temp1 = temp1->next;
+
+    temp1->next = tail->next;
+    free(tail);
+    tail = temp1;
+
+    return tail;
+}
+
+// ---delete a node at a certain position---
+struct node *delAnyPos(struct node *tail, int pos)
+{
+    if (tail == NULL)
+        return NULL;
+    if (tail->next == tail)
+    {
+        free(tail);
+        tail = NULL;
+        return NULL;
+    }
+    struct node *temp1, *temp2;
+    temp1 = tail->next;
+    if (pos == 1)
+    {
+        tail->next = temp1->next;
+        free(temp1);
+        temp1 = NULL;
+        return tail;
+    }
+    pos--;
+    while (--pos)
+        temp1 = temp1->next;
+    temp2 = temp1->next;
+    temp1->next = temp2->next;
+    if (temp2 == tail)
+        tail = temp1;
+    free(temp2);
+    temp2 = NULL;
+    return tail;
+}
+
 void main()
 {
     struct node *tail;
 
     // add node in circular singly linked list
-    tail = addToEmpty(30);            // Time Complexity = O(1)
-    tail = addAtBeg(tail, 20);        // Time Complexity = O(1)
-    tail = addAtEnd(tail, 40);        // Time Complexity = O(1)
-    tail = addAtBeg(tail, 10);        // Time Complexity = O(1)
-    tail = addAfterPos(tail, 15, 1);  // Time Complexity = O(n)
-    tail = addBeforePos(tail, 25, 4); // Time Complexity = O(n)
+    tail = addToEmpty(30);            // Time Complexity = O(1)     Data = 30
+    tail = addAtBeg(tail, 20);        // Time Complexity = O(1)     Data = 20 30
+    tail = addAtEnd(tail, 40);        // Time Complexity = O(1)     Data = 20 30 40
+    tail = addAtBeg(tail, 10);        // Time Complexity = O(1)     Data = 10 20 30 40
+    tail = addAfterPos(tail, 15, 1);  // Time Complexity = O(n)     Data = 10 15 20 30 40
+    tail = addBeforePos(tail, 25, 4); // Time Complexity = O(n)     Data = 10 15 20 25 30 40
 
     // OutPut: 10 15 20 25 30 40
-    print(tail); // Time Complexity = O(n)
+    print(tail);      // Time Complexity = O(n)
+    countNodes(tail); // Time Complexity = O(n)     List Length = 3
+
+    // deletion operations
+    tail = delFirstNode(tail); // Time Complexity = O(1)    Data = 15 20 25 30 40
+    tail = delLastNode(tail);  // Time Complexity = O(n)    Data = 15 20 25 30
+    tail = delAnyPos(tail, 2); // Time Complexity = O(n)    Data = 15 25 30
+
+    print(tail);      // Data = 15 25 30
+    countNodes(tail); // Time Complexity = O(n)     List Length = 3
+
+    search(tail, 30); // Time Complexity = O(n)     30 found in node 3!
 }
